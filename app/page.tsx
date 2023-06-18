05ed852bfbf864834599ababa00a94c7d7f99827
Geoffrey Coste <geoffreycoste13@gmail.com>
2023-06-18 18:27:56 +0200

    Responsive adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <Modal>
         <Searchbar placeholder="Search location" />
       </Modal>
-      <div className="w-1/2 h-1/2 flex flex-col gap-x-2 bg-blue-700 p-2 mb-2 rounded-lg shadow">
+      <div className="w-full md:w-1/2 h-1/2 flex flex-col gap-x-2 bg-blue-700 p-2 mb-2 rounded-lg shadow">
         <h2 className="text-white text-lg font-bold mb-4">Suggested cities</h2>
         <div className="w-full h-full flex gap-x-2">
           <div className="basis-1/4 h-full bg-slate-200 rounded-lg p-4">City 1</div>
